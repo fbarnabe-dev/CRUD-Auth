629f5037513447d2b23765659e3b1f0c4737187f
Felipe Barnabe <fbarnabe8@gmail.com>
2025-01-27 17:17:29 -0300

    Projeto CRUD com autenticação Asp.Net Identity

---
+++ Pages/Premiums/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CRUD.Data;
+using CRUD.models;
+
+namespace CRUD.Pages_Premiums
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CRUD.Data.ApplicationDbContext _context;
+
+        public IndexModel(CRUD.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Premium> Premium { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Premium = await _context.Premiums
+                .Include(p => p.Student).ToListAsync();
+        }
+    }
+}
---
+++ models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.models;
+
+public class Student
+{
+    [Key]
+    [DisplayName("Id")]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe o nome")]
+    [StringLength(80, ErrorMessage = "O nome deve conter até 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O nome deve conter pelo menos 5 caracteres")]
+    [DisplayName("Nome Completo")]
+    public string Name { get; set; } = String.Empty;
+    
+    [Required(ErrorMessage = "Innforme o E-mail")]
+    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; } = String.Empty;
+
+    public List<Premium> Premiums { get; set; } = new();
+}
